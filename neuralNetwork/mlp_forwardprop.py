e48f59e670dd883d4bcd66a4e03b38f0c7cf8248
SohyunKim <chloek9649@gmail.com>
2020-09-29 11:37:07 +0900

    initialize mlp source code

---
+++ neuralNetwork/mlp_forwardprop.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class MLP:
+
+    # define 3 attributes
+    def __init__(self, num_inputs, num_hidden, num_outputs):
+        self.num_inputs = num_inputs
+        self.num_hidden = num_hidden
+        self.num_outputs = num_outputs
